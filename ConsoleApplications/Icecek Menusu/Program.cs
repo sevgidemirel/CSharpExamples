fbdbbf14c27097a81a6131449609aaf63d03dc73
Sevgi Demirel <demirelsevgi@outlook.com>
2017-11-11 20:08:01 +0300

    console app added

---
+++ ConsoleApplications/Icecek Menusu/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icecek_Menusu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float toplamTutar = 0;
+            ConsoleKeyInfo tus = new ConsoleKeyInfo();
+
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine("1- Su : 1.00 TL");
+                Console.WriteLine("2- Kola : 4.00 TL");
+                Console.WriteLine("3- Türk Kahvesi : 5.00 TL");
+                Console.WriteLine("4- Soda : 0.50 TL");
+                Console.WriteLine("5- Çay : 1.00 TL");
+                Console.WriteLine("6- Meyve Suyu : 2.50 TL");
+
+                Console.WriteLine("-----------------------");
+                Console.Write("Bir Seçim Yapınız: ");
+                try 
+                {
+                    byte sayi = byte.Parse(Console.ReadLine());
+                    switch (sayi)
+                    {
+                        case 1: toplamTutar += (float)1.00; break;
+                        case 2: toplamTutar += (float)4.00; break;
+                        case 3: toplamTutar += (float)5.00; break;
+                        case 4: toplamTutar += (float)0.50; break;
+                        case 5: toplamTutar += (float)1.00; break;
+                        case 6: toplamTutar += (float)2.50; break;
+                        default:
+                            Console.Write("Geçersiz seçim!!..");
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("\nHatanız: {0} \n",e);
+                }
+
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("\nBaşka Bir Şey İster Misiniz (e/h) ? : ");
+                tus = Console.ReadKey();
+
+
+                if (tus.Key == ConsoleKey.H)
+                    Console.Write("\nToplam Tutarınız : {0} Tl, iyi günler!!", toplamTutar);
+  
+
+            } while (tus.Key == ConsoleKey.E);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApplications/Street Fight/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Street_Fight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int kenCan = 50, zyraCan = 50;
+            byte kenStep = 0, zyraStep = 0;
+
+         
+
+            do
+            {
+                int sira = random.Next(0, 2);
+                Console.Clear();
+                Console.Write("Z: {0} ", zyraCan);
+                for (int i = 0; i < zyraCan; i++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("K: {0} ", kenCan);
+                for (int i = 0; i < kenCan; i++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.Write(" ");
+                }
+                if (sira == 0)
+                {
+                    kenStep++;
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    int kenVurus = random.Next(1, 10);
+                    Console.Write("\n{0}. Ken ==> {1} vurdu!! \n",kenStep, kenVurus);
+                    zyraCan -= kenVurus;
+                    if (zyraCan <= 0)
+                    {
+                        Console.Write("{0} adımda {1} canla 'KEN' kazandı", kenStep, kenCan);
+                        break;
+                    }
+                    Console.ReadKey();
+
+                }
+                else if (sira == 1)
+                {
+                    zyraStep++;
+
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    int zyraVurus = random.Next(1, 10);
+                    Console.Write("\n{0}. Zyra ==> {1} vurdu!! \n",zyraStep, zyraVurus);
+                    kenCan -= zyraVurus;
+                    if (kenCan <= 0)
+                    {
+                        Console.Write("{0} adımda {1} canla 'ZYRA' kazandı",zyraStep, zyraCan);
+                        break;
+                    }
+                    Console.ReadKey();
+
+                }
+            } while (true);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
